d251f965a04f329c4d0d297eca913024c87b840e
Gold Okpa <okpagold@gmail.com>
2024-07-29 22:43:20 +0100

    Add Stripe payment functionality - Added PaymentController with Index, CreateCheckout, Success, and Cancel actions - Created views for payment process: Index.cshtml, Success.cshtml, Cancel.cshtml - Configured Stripe API key in appsettings.json - Updated Program.cs for routing and Stripe configuration

---
+++ StripeWebAppication/StripeWebApp/Controllers/PaymentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+
+namespace StripeWebApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCheckout()
+        {
+            var domain = "http://localhost:7086";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = "{{PRICE_ID}}",
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/Payment/Success",
+                CancelUrl = domain + "/cancel/Success",
+                AutomaticTax = new SessionAutomaticTaxOptions { Enabled = true },
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
--- StripeWebAppication/StripeWebApp/Program.cs
+++ StripeWebAppication/StripeWebApp/Program.cs
@@ -1,3 +1,5 @@
+using Stripe;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,14 +12,14 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); 
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 app.UseRouting();
-
+ 
 app.UseAuthorization();
 
 app.MapControllerRoute(
